25bd141103e598e8c5c1e6a479ce588cc25a4c7e
Matthew <matthewtindogan72@gmail.com>
2025-12-02 18:53:00 -0800

    Part 7: implemented singleton pattern for BorrowingService

--- src/lab5/BorrowingService.java
+++ src/lab5/BorrowingService.java
@@ -1,36 +1,53 @@
 package lab5;
 
 public class BorrowingService implements BorrowingServiceAPI {
+    private static BorrowingService instance;
+    private int borrowingLimit;
+
+    private BorrowingService() {
+        borrowingLimit = 3;
+    }
+
+    public static synchronized BorrowingService getInstance() {
+        if(instance == null){
+            instance = new BorrowingService();
+        }
+        return instance;
+    }
+
     @Override
     public BorrowingBookResult borrowBook(Member member, Book book) {
-        if(book.getIsAvailable() && member.borrowedBooksCount() < 3) {
-            book.setIsAvailable(false);
-            System.out.println("Borrowing book: " + book);
-            member.getBorrowedBooks().add(book);
-            return new BorrowingBookResult(true, "Borrowed book");
-        }
-        if(member.borrowedBooksCount() >= 3) {
-            return new BorrowingBookResult(false, "Too many books borrowed");
-        }
-        if(member.getBorrowedBooks().contains(book)) {
+        if(member.getBorrowedBooks().contains(book)){
             return new BorrowingBookResult(false, "This member has already borrowed the book");
         }
 
-        return new BorrowingBookResult(false, "Another member has borrowed the book");
+        if(member.borrowedBooksCount() >= borrowingLimit){
+            return new BorrowingBookResult(false, "Too many borrowed books");
+        }
+
+        if(!book.getIsAvailable()){
+            return new BorrowingBookResult(false, "This book is borrowed by another member");
+        }
+
+        book.setIsAvailable(false);
+        System.out.println("Borrowing book: " + book);
+        member.getBorrowedBooks().add(book);
+        return new BorrowingBookResult(true, "Borrowed book: " + book);
     }
 
     @Override
     public BorrowingBookResult returnBook(Member member, Book book) {
-        if(!book.getIsAvailable() && member.getBorrowedBooks().contains(book)) {
-            book.setIsAvailable(true);
-            System.out.println("Returning book: " + book);
-            member.getBorrowedBooks().remove(book);
-            return new BorrowingBookResult(true, "Returned book");
-        }
-        if(!member.getBorrowedBooks().contains(book)) {
-            return new BorrowingBookResult(false, "Book has not been borrowed");
+        if(!member.getBorrowedBooks().contains(book)){
+            return new BorrowingBookResult(false, "This book is not borrowed by another member");
         }
 
-        return new BorrowingBookResult(false, "Book has already been returned");
+        if(book.getIsAvailable()){
+            return new BorrowingBookResult(false, "This book has been returned");
+        }
+
+        book.setIsAvailable(true);
+        System.out.println("Returning book: " + book);
+        member.getBorrowedBooks().remove(book);
+        return new BorrowingBookResult(true, "Returned book: " + book);
     }
 }
